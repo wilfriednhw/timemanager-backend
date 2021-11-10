e20e89755c4f586786cbabc541693fa67d2ee0a7
wilfriednhw <wilfriedhermanno@gmail.com>
2021-11-10 16:55:08 +0100

    fix bugs ci/cd

--- config/test.exs
+++ config/test.exs
@@ -6,6 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :moodle, Moodle.Repo,
+
   username: "postgres",
   password: "postgres",
   database: "moodledb",
