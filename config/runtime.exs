1238cfe0d03f2829f11c0dddccf51290755c7afc
wilfriednhw <wilfriedhermanno@gmail.com>
2021-11-12 12:56:04 +0100

    fix bugs ci/cd heroku

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,7 +16,7 @@ if config_env() == :prod do
 
   config :moodle, Moodle.Repo,
     ssl: true,
-    # socket_options: [:inet6],
+    socket_options: [:inet6],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
@@ -39,6 +39,7 @@ if config_env() == :prod do
       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      transport_options: [socket_opts: [:inet6]]
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
     secret_key_base: secret_key_base
