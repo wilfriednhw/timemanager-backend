b8284848053164eacfa6831deec849add81b08ca
Hermann Nkamwa <wilfriedhermanno@gmail.com>
2021-11-16 08:32:01 +0100

    fix bugs

--- lib/moodle/release.ex
+++ lib/moodle/release.ex
@@ -1,23 +1,24 @@
 defmodule Moodle.Release do
-  """
-  Release script for running migrations. migrate function runs all migrations
-  """
   @app :moodle
 
   def migrate do
-    # Get all repos and run ecto migrate
+    load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
-  # loads all repos for our app
   defp repos do
-    Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
   end
 end
