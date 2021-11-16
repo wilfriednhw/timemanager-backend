601873efbf53c61b1ee7d8d0f2e6f0ca05b1dffb
Hermann Nkamwa <wilfriedhermanno@gmail.com>
2021-11-16 19:28:20 +0100

    fix bugs ci/cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # Docker entrypoint script.
 
+exec ls
 
-
-bin="/app/bin/moodle"
-eval "$bin" "Moodle.Release.migrate"
-exec "$bin" "start"
+# bin="/app/bin/moodle"
+# eval "$bin" "Moodle.Release.migrate"
+# exec "$bin" "start"
 
 
 
