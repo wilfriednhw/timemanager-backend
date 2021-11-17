29cb72aef29b7e8f4a7f4e65ba687a8501eb747b
Hermann Nkamwa <wilfriedhermanno@gmail.com>
2021-11-17 11:50:55 +0100

    fix bugs ci/cd

--- lib/moodle/release.ex
+++ lib/moodle/release.ex
@@ -2,22 +2,23 @@ defmodule Moodle.Release do
   @app :moodle
 
   def migrate do
+    load_app()
+
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
     end
   end
 
   def rollback(repo, version) do
+    load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
   end
 
   defp repos do
-    Application.load(@app)
     Application.fetch_env!(@app, :ecto_repos)
   end
 
-  defp ensure_started do
-    Application.ensure_all_started(:ssl)
+  defp load_app do
+    Application.load(@app)
   end
-
 end
